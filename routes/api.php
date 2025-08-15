6814ffd13076db34fd548c9a0759e75a37b618c4
samuelinux <samuelpereiramachado@gmail.com>
2025-08-15 16:55:19 -0300

    incluir url publica para verificar disponibilidades

--- routes/api.php
+++ routes/api.php
@@ -15,16 +15,18 @@ use App\Http\Controllers\Api\AgendamentoController;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // Rotas públicas (para os modais)
 Route::get('/servicos', [AgendamentoController::class, 'servicos']);
+Route::get('/horarios-disponiveis', [AgendamentoController::class, 'horariosDisponiveis']);
 
 // Rotas protegidas por autenticação
 Route::middleware('auth')->group(function () {
     Route::get('/datas-disponiveis', [AgendamentoController::class, 'datasDisponiveis']);
-    Route::get('/horarios-disponiveis', [AgendamentoController::class, 'horariosDisponiveis']);
+
     Route::post('/agendamentos', [AgendamentoController::class, 'store']);
 });
